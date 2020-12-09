ae6759c21ca248f974343364ba8b52750967a18b
D. Wilson <34779401+dwilson-coder@users.noreply.github.com>
2020-12-09 05:52:15 -0500

    Add files via upload

---
+++ js/quiz.js
@@ -0,0 +1,41 @@
+function submitAnswers(){
+	var total = 5;
+	var score = 0;
+	
+	// Get User Input
+	var q1 = document.forms["quizForm"]["q1"].value;
+	var q2 = document.forms["quizForm"]["q2"].value;
+	var q3 = document.forms["quizForm"]["q3"].value;
+	var q4 = document.forms["quizForm"]["q4"].value;
+	var q5 = document.forms["quizForm"]["q5"].value;
+	
+	// Validation
+	for(i = 1; i <= total;i++){
+		if(eval('q'+i) == null || eval('q'+i) == ''){
+			alert('You missed question '+ i);
+			return false;
+		}
+	}
+	
+	// Set Correct Answers
+	var answers = ["b","a","d","b","d"];
+	
+	// Check Answers
+	for(i = 1; i <= total;i++){
+		if(eval('q'+i) == answers[i - 1]){
+			score++;
+		}
+	}
+	
+	// Display Results in the footer section of the card   
+        var questions = document.getElementById('total');
+        var results = document.getElementById('correct');
+        var failed = 5 - score;
+
+        questions.innerHTML = total;
+        results.innerHTML = score;
+        wrong.innerHTML = failed;  
+
+
+	return false;
+}
